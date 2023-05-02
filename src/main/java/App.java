ef290e14b20fd4edc4def51620e21c0393d307eb
MikeStrat <erikshaydulin@gmail.com>
2023-05-02 12:06:28 +0500

    Added Cat.class + created catBean.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        //по заданию
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(cat == cat2);
+
+
+
+
     }
 }
